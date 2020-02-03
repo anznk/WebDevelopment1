9217dd6f91f5719ae81d6eea845f24facc1e98e5
Anzu <anzunakayama@gmail.com>
2020-02-03 11:48:28 -0800

    20200203

---
+++ src/js/contact.js
@@ -0,0 +1,16 @@
+/* validation checks
+---------------------------------- */
+var subForm = document.getElementById('subForm');
+
+// err message
+var STRING_EMPTY = '※入力が必要です。';
+
+//click submit button
+subForm.addEventListener('click', function() {
+    // get element
+    var company = document.getElementById("company").value;
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+
+
+}, false);

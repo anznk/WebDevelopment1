0b4b77dfbfb211fa8f463a27d63abfa7913b53f7
Anzu <anzunakayama@gmail.com>
2020-02-06 18:43:38 -0800

    20200206-2

--- src/js/contact.js
+++ src/js/contact.js
@@ -3,7 +3,9 @@
 let subForm = document.getElementById('subForm');
 
 // err message
-const STRING_EMPTY = 'This field is required';
+const STRING_EMP_NAME = 'Please enter your name!!';
+const STRING_EMP_EMAIL = 'Please enter your e-mail address!!';
+const STRING__INVALID_EMAIL = 'Please enter valid e-mail address!!';
 
 //click submit button
 // subForm.addEventListener('click', function() {
@@ -22,15 +24,24 @@ const STRING_EMPTY = 'This field is required';
 let errmsg = document.getElementById("name_msg");
 
 subForm.addEventListener('click', function() {
+    let name_box = document.getElementById('name');
+    let email_box = document.getElementById('email');
+    let name_msg = document.getElementById('name_msg');
+    let email_msg = document.getElementById('email_msg');
 
-
-    let mandatory = document.getElementsByClassName('mandatory')[0].value;
-
-    if (mandatory == "") {
-        alert(mandatory);
-        errmsg.innerHTML = STRING_EMPTY;
+    if (name_box.value == "") {
+        name_msg.classList.remove('msg');
+        name_msg.classList.add('err_msg');
+        name_msg.innerHTML = STRING_EMP_NAME;
     }
-
-
+    if (email_box.value == "") {
+        email_msg.classList.remove('msg');
+        email_msg.classList.add('err_msg');
+        email_msg.innerHTML = STRING_EMP_EMAIL;
+    } else if (!email_box.value.includes('@')) {
+        email_msg.classList.remove('msg');
+        email_msg.classList.add('err_msg');
+        email_msg.innerHTML = STRING__INVALID_EMAIL;
+    }
 
 }, false);
